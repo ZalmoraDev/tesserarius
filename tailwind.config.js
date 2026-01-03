ffb9fb0af1781bc7997fe82b4ebddf830f428c2f
Stef Visser <45939670+ZalmoraDev@users.noreply.github.com>
2026-01-03 13:52:05 +0100

    Commit before removing Vite (Unnecessary)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@
 export default {
     content: [
         './app/view/**/*.php',       // all php files containing Tailwind classes
+        './app/view/skeleton/**/*.php',       // all php files containing Tailwind classes
+        './app/view/components/**/*.php',       // all php files containing Tailwind classes
         './app/resources/js/**/*.js', // any JS files using Tailwind classes
     ],
     theme: {
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     build: {
-        outDir: './app/public/assets',
-        emptyOutDir: true,
+        outDir: './app/public',
+        emptyOutDir: false,
         manifest: true,
         rollupOptions: {
             input: 'app/resources/js/app.js'
